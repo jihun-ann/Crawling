367be14647891e4a4d9dfe094e575ca0ab1a1c2f
jihunan <dasio722@gmail.com>
2026-01-22 16:50:03 +0900

    Seaching TEST

--- src/src/spiders/NaverSpider.py
+++ src/src/spiders/NaverSpider.py
@@ -1,42 +1,54 @@
-import os
-import sys
 import urllib.request
 import json
 from scrapy import Spider
 from importlib import resources
 from collections import deque
-
+from bs4 import BeautifulSoup
 
 class NaverSpider(Spider):
     name = "naver_crawling"
 
     def start_requests(self):
-        location_q = self.location_name(self.prov)
-        keyword_q = self.keyword_list()
+        province = self.prov
+        location_q = self.location_name(province)
+
+        keyword_q = deque()
+        keyword_q.append("맛집")
+        #keyword_q = self.keyword_list()
+
         for location in location_q:
             for keyword in keyword_q:
                 search_query = " ".join([location,keyword])
-                self.searching_naver_map(search_query)
+                self.searching_naver_blog(search_query)
                 #self.searching_naver_blog(search_query)
 
-    def searching_naver_map(self, query):
+    def searching_naver_blog(self, query):
         re_query = urllib.parse.quote(query)
-        print(re_query)
-        #url = "https://search.naver.com/search.naver?query=" + re_query                         #전체 통합 검색
+        #url = "https://search.naver.com/search.naver?query=" + re_query                       #전체 통합 검색
         url = "https://search.naver.com/search.naver?ssc=tab.blog.all&query=" + re_query       #블로그 검색
-        #url = "https://search.naver.com/search.naver?ssc=tab.cafe.all&query=" + re_query       #카페 검색
-        print(url)
+        #url = "https://search.naver.com/search.naver?ssc=tab.cafe.all&query=" + re_query      #카페 검색
+
         request = urllib.request.Request(url)
         response = urllib.request.urlopen(request)
         rescode = response.getcode()
         if(rescode==200):
             response_body = response.read()
-            print(query)
-            #print(response_body.decode('utf-8'))
+            self.parsing_html(response_body.decode('utf-8'))
         else:
             print("Error Code:" + rescode)
 
-    def searching_naver_blog(self,query):
+    def parsing_html(self,html):
+        soup = BeautifulSoup(html,"html.parser")
+        for span in soup.select("span.sds-comps-text-type-headline1"):
+            text = span.get_text()
+            if(text in ("경매", "법원경매", "부동산")) :
+                continue
+            else :
+                a = span.find_parent("a")
+                print(a.get("href"))
+                pass
+
+    def searching_naver_api_blog(self,query):
         client_id = "lBn2OlsTkSAo6Dyr6h1c"
         client_secret = "rFc99bozMu"
         re_query = urllib.parse.quote(query)
